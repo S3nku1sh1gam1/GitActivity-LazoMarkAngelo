a3b20e336e938f86e816e538e0dfe91c983949d5
LazoMarkAngelo <lazomarkangelo3@gmail.com>
2023-03-09 23:13:19 +0800

    My Personal Info added

---
+++ GitActivity-LazoMarkAngelo.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar  9 23:04:19 2023
+
+@author: lazom
+"""
+
---
+++ git-python-lazo.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar  9 23:07:14 2023
+
+@author: lazom
+"""
+
+print("Name: Mark Angelo Lazo")
+
+print("Address: Mataas na Bayan,Mogpog, Marinduque")
+
+print("Birthdate: January 14, 2003")
