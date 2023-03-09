45c661b86897803b19df31764eb592d1f86ed294
S3nku1sh1gam1 <127424873+S3nku1sh1gam1@users.noreply.github.com>
2023-03-10 00:18:48 +0800

    Add files via upload

---
+++ git-python-lazo.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar  9 23:07:14 2023
+
+@author: lazom
+"""
+
+print("Name: Mark Angelo Lazo")
+
+print("Address: Mataas na Bayan,Mogpog, Marinduque")
+
+print("Birthdate: January 14, 2003")
+
+print("Colloge: Marinduque, State College")
+
+print("Course: Bachelor of Science in Information and Technology ")
+
+print("Year and Section: 2B")
+
+print("My Hobbies are: Playing online games, Tabble Tennis, Basket Ball, Badminton, Playing guitar")
